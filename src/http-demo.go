105817d358b7ea60ecdf8a48aed3cd81cd1efabe
mrmuli <jayjaymuli00@gmail.com>
2022-11-18 01:17:23 +0100

    feat: Add feature flag toggle demo

--- src/http-demo.go
+++ src/http-demo.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/Unleash/unleash-client-go"
 	"net/http"
 )
 
+func init() {
+	unleash.Initialize(
+		unleash.WithListener(&unleash.DebugListener{}),
+		unleash.WithAppName("http-demo"),
+		unleash.WithUrl("http://localhost:4242/api/"),
+		unleash.WithCustomHeaders(http.Header{"Authorization": {"very-secret-token"}}),
+	)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Congratulations! Your Go application is running!.")
+	if unleash.IsEnabled("http-demo.hello") {
+		// io.WriteString(w, "Congratulations! this feature is enabled\n")
+		fmt.Fprintf(w, "Congratulations! this feature is enabled\n")
+	} else {
+		// io.WriteString(w, "Hello this feature isn't there yet!\n")
+		fmt.Fprintf(w, "Hello this feature isn't there yet!\n")
+	}
 }
 
 func main() {
